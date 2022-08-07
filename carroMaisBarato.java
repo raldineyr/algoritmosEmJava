396579069ce5b61f3157e5318896f4937938a5f1
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2022-08-07 00:14:47 -0300

    ENCONTRAR O MENOR VALOR

---
+++ carroMaisBarato.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int[] carro = { 1000000, 40000, 16000, 46000, 17000 };
+
+    int carroMaisBarato = 0;
+    int atual;
+
+    for (atual = 0; atual <= (carro.length -1); atual++) {
+      if (carro[atual] < carro[carroMaisBarato]) {
+        carroMaisBarato = atual;
+      }
+    }
+    System.out.println("\n\n\tCarro mais barato: " + carroMaisBarato);
+  }
+}
