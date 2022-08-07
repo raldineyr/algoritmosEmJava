c0680ee1651e7dacee72b8135c26ea28ee2eb360
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2022-08-07 11:31:45 -0300

    Update carroMaisBarato.java

--- carroMaisBarato.java
+++ carroMaisBarato.java
@@ -1,11 +1,12 @@
 class Main {
   public static void main(String[] args) {
-    int[] carro = { 1000000, 40000, 16000, 46000, 17000 };
+    int[] precos = { 1000000, 40000, 16000, 46000, 17000 };
 
     int carroMaisBarato = 0;
 
-    for (int atual = 0; atual <= (carro.length -1); atual++) {
-      if (carro[atual] < carro[carroMaisBarato]) {
+
+    for (int atual = 0; atual <= (precos.length -1); atual++) {
+      if (precos[atual] < precos[carroMaisBarato]) {
         carroMaisBarato = atual;
       }
     }
