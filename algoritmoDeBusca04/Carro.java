7f77b22774f0bed97baa932c867827a0b51eea6e
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2022-08-09 20:14:48 -0300

    Classe Carro

---
+++ algoritmoDeBusca04/Carro.java
@@ -0,0 +1,27 @@
+class Carro
+{
+
+  private String nome;
+  private double preco;
+
+  public Carro (String nome, double preco){
+    this.nome = nome;
+    this.preco = preco;
+  }
+
+  public String getNome(){
+   return nome; 
+  }
+  
+  public double getPreco(){
+  return preco;
+  }
+
+
+  public String toString()
+  {
+    return getNome() + ", R$ " + getPreco();
+  }
+
+  
+}
